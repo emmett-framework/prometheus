c65081b6413a0e78bda230fe921244be52cc9f2d
Giovanni Barillari <g@baro.dev>
2024-10-13 18:25:20 +0200

    Support `emmett-core` extensions

---
+++ emmett_prometheus/_imports.py
@@ -0,0 +1,4 @@
+try:
+    from emmett import request, response, websocket
+except ImportError:
+    from emmett55 import request, response, websocket
--- emmett_prometheus/ext.py
+++ emmett_prometheus/ext.py
@@ -1,34 +1,34 @@
 import time
 
 import prometheus_client
+from emmett_core.datastructures import sdict
+from emmett_core.extensions import Extension, Signals, listen_signal
+from emmett_core.pipeline.pipe import Pipe
+from emmett_core.routing.rules import HTTPRoutingRule, WebsocketRoutingRule
 
-from emmett import Pipe, request, response, websocket, sdict
-from emmett.extensions import Extension, Signals, listen_signal
-from emmett.routing.rules import HTTPRoutingRule, WebsocketRoutingRule
+from ._imports import request, response, websocket
 
 
 class Prometheus(Extension):
-    default_config = dict(
-        auto_load=True,
-        enable_http_metrics=True,
-        enable_ws_metrics=True,
-        enable_sys_metrics=False,
-        metrics_route_path="/metrics",
-        metrics_route_hostname=None,
-        metrics_names={
+    default_config = {
+        "auto_load": True,
+        "enable_http_metrics": True,
+        "enable_ws_metrics": True,
+        "enable_sys_metrics": False,
+        "metrics_route_path": "/metrics",
+        "metrics_route_hostname": None,
+        "metrics_names": {
             "http_counter": "emmett_request_count",
             "http_histogram": "emmett_request_latency",
             "ws_gauge": "emmett_websocket_gauge",
             "ws_recv_counter": "emmett_websocket_messages_recv_count",
-            "ws_send_counter": "emmett_websocket_messages_send_count"
+            "ws_send_counter": "emmett_websocket_messages_send_count",
         },
-        http_histogram_buckets=[
-            5, 35, 100, 200, 500, 1000, "INF"
-        ],
-        http_histogram_statuses=[200, 201],
-        http_histogram_exclude_methods=["OPTIONS"],
-        exclude_routes=[]
-    )
+        "http_histogram_buckets": [5, 35, 100, 200, 500, 1000, "INF"],
+        "http_histogram_statuses": [200, 201],
+        "http_histogram_exclude_methods": ["OPTIONS"],
+        "exclude_routes": [],
+    }
 
     def on_load(self):
         self._excluded_routes = set(self.config.exclude_routes)
@@ -43,48 +43,37 @@ class Prometheus(Extension):
 
         if self.config.enable_http_metrics:
             self.metrics.http_counter = prometheus_client.Counter(
-                self.config.metrics_names["http_counter"],
-                "Requests count",
-                labelnames=["route", "method", "status"]
+                self.config.metrics_names["http_counter"], "Requests count", labelnames=["route", "method", "status"]
             )
             self.metrics.http_histogram = prometheus_client.Histogram(
                 self.config.metrics_names["http_histogram"],
                 "Requests latency histogram (ms)",
                 labelnames=["route", "method", "status"],
-                buckets=self.config.http_histogram_buckets
+                buckets=self.config.http_histogram_buckets,
             )
             self._pipe_http = PrometheusHTTPPipe(self)
 
         if self.config.enable_ws_metrics:
             self.metrics.ws_gauge = prometheus_client.Gauge(
-                self.config.metrics_names["ws_gauge"],
-                "Websockets connection gauge",
-                labelnames=["websocket"]
+                self.config.metrics_names["ws_gauge"], "Websockets connection gauge", labelnames=["websocket"]
             )
             self.metrics.ws_counter_recv = prometheus_client.Counter(
                 self.config.metrics_names["ws_recv_counter"],
                 "Websockets received messages counter",
-                labelnames=["websocket"]
+                labelnames=["websocket"],
             )
             self.metrics.ws_counter_send = prometheus_client.Counter(
                 self.config.metrics_names["ws_send_counter"],
                 "Websockets sent messages counter",
-                labelnames=["websocket"]
+                labelnames=["websocket"],
             )
             self._pipe_ws = PrometheusWSPipe(self)
 
         if self.config.auto_load:
-            self.appmod = self.app.module(
-                __name__,
-                "emmett_prometheus",
-                hostname=self.config.metrics_route_hostname
+            self.appmod = self.app.module(__name__, "emmett_prometheus", hostname=self.config.metrics_route_hostname)
+            self.appmod.route(self.config.metrics_route_path, name="metrics", methods="get", output="bytes")(
+                self._metrics_route
             )
-            self.appmod.route(
-                self.config.metrics_route_path,
-                name="metrics",
-                methods='get',
-                output='bytes'
-            )(self._metrics_route)
 
     @listen_signal(Signals.before_route)
     def _inject_pipes(self, route, f):
@@ -127,29 +116,13 @@ class PrometheusHTTPPipe(Pipe):
         request._prometheus_http_histogram_ts = time.perf_counter_ns()
 
     async def close_request(self):
-        self._http_counter.labels(
-            route=request.name,
-            method=request.method,
-            status=response.status
-        ).inc()
-        if (
-            self.ext._httph_filter_methods and
-            request.method in self.ext._httph_filter_methods
-        ):
+        self._http_counter.labels(route=request.name, method=request.method, status=response.status).inc()
+        if self.ext._httph_filter_methods and request.method in self.ext._httph_filter_methods:
             return
-        if (
-            self.ext._httph_only_status and
-            request.method not in self.ext._httph_only_status
-        ):
+        if self.ext._httph_only_status and request.method not in self.ext._httph_only_status:
             return
-        self._http_histogram.labels(
-            route=request.name,
-            method=request.method,
-            status=response.status
-        ).observe(
-            (
-                time.perf_counter_ns() - request._prometheus_http_histogram_ts
-            ) / 1_000_000
+        self._http_histogram.labels(route=request.name, method=request.method, status=response.status).observe(
+            (time.perf_counter_ns() - request._prometheus_http_histogram_ts) / 1_000_000
         )
 
 
